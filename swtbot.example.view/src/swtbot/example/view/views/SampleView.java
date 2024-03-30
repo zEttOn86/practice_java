d92f5a15d1da109f659a14c87e711020d0e6a5bf
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2024-03-30 22:37:36 +0900

    修正

--- swtbot.example.view/src/swtbot/example/view/views/SampleView.java
+++ swtbot.example.view/src/swtbot/example/view/views/SampleView.java
@@ -1,33 +1,33 @@
 package swtbot.example.view.views;
 
-
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.*;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.action.*;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.*;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.TreeEvent;
+
+import java.util.ArrayList;
+
 import javax.inject.Inject;
 
-
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
+ * This sample class demonstrates how to plug-in a new workbench view. The view
+ * shows data obtained from the model. The sample creates a dummy model on the
+ * fly, but a real implementation would connect to the model available either in
+ * this or another plug-in (e.g. the workspace). The view is connected to the
+ * model using a content provider.
  * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
+ * The view uses a label provider to define how model objects should be
+ * presented in the view. Each view can present the same model objects using
+ * different labels and icons, if needed. Alternatively, a single label provider
+ * can be shared between views in order to ensure that objects of the same type
+ * are presented in the same way everywhere.
  * <p>
  */
 
@@ -38,39 +38,171 @@ public class SampleView extends ViewPart {
 	 */
 	public static final String ID = "swtbot.example.view.views.SampleView";
 
-	@Inject IWorkbench workbench;
-	
-	private TableViewer viewer;
+	@Inject
+	IWorkbench workbench;
+
+	private TreeViewer viewer;
+	private DrillDownAdapter drillDownAdapter;
 	private Action action1;
 	private Action action2;
 	private Action doubleClickAction;
-	 
 
-	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
-		@Override
-		public String getColumnText(Object obj, int index) {
-			return getText(obj);
+	class TreeObject implements IAdaptable {
+		private String name;
+		private TreeParent parent;
+
+		public TreeObject(String name) {
+			this.name = name;
 		}
-		@Override
-		public Image getColumnImage(Object obj, int index) {
-			return getImage(obj);
+
+		public String getName() {
+			return name;
 		}
-		@Override
-		public Image getImage(Object obj) {
-			return workbench.getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
+
+		public void setParent(TreeParent parent) {
+			this.parent = parent;
+		}
+
+		public TreeParent getParent() {
+			return parent;
+		}
+
+		public String toString() {
+			return getName();
+		}
+
+		public <T> T getAdapter(Class<T> key) {
+			return null;
 		}
 	}
 
-	@Override
-	public void createPartControl(Composite parent) {
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		
-		viewer.setContentProvider(ArrayContentProvider.getInstance());
-		viewer.setInput(new String[] { "One", "Two", "Three" });
-	viewer.setLabelProvider(new ViewLabelProvider());
+	class TreeParent extends TreeObject {
+		private ArrayList<TreeObject> children;
 
-		// Create the help context id for the viewer's control
-		workbench.getHelpSystem().setHelp(viewer.getControl(), "swtbot.example.view.viewer");
+		public TreeParent(String name) {
+			super(name);
+			children = new ArrayList<>();
+		}
+
+		public void addChild(TreeObject child) {
+			children.add(child);
+			child.setParent(this);
+		}
+
+		public void removeChild(TreeObject child) {
+			children.remove(child);
+			child.setParent(null);
+		}
+
+		public TreeObject[] getChildren() {
+			return (TreeObject[]) children.toArray(new TreeObject[children.size()]);
+		}
+
+		public boolean hasChildren() {
+			return children.size() > 0;
+		}
+	}
+
+	class ViewContentProvider implements IStructuredContentProvider, ITreeContentProvider {
+		private TreeParent invisibleRoot;
+
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+		}
+
+		public void dispose() {
+		}
+
+		public Object[] getElements(Object parent) {
+			if (parent.equals(getViewSite())) {
+				if (invisibleRoot == null)
+					initialize();
+				return getChildren(invisibleRoot);
+			}
+			return getChildren(parent);
+		}
+
+		public Object getParent(Object child) {
+			if (child instanceof TreeObject) {
+				return ((TreeObject) child).getParent();
+			}
+			return null;
+		}
+
+		public Object[] getChildren(Object parent) {
+			if (parent instanceof TreeParent) {
+				return ((TreeParent) parent).getChildren();
+			}
+			return new Object[0];
+		}
+
+		public boolean hasChildren(Object parent) {
+			if (parent instanceof TreeParent)
+				return ((TreeParent) parent).hasChildren();
+			return false;
+		}
+
+		/*
+		 * We will set up a dummy model to initialize tree heararchy. In a real code,
+		 * you will connect to a real model and expose its hierarchy.
+		 */
+		private void initialize() {
+			TreeObject to1 = new TreeObject("Leaf 1");
+			TreeObject to2 = new TreeObject("Leaf 2");
+			TreeObject to3 = new TreeObject("Leaf 3");
+			TreeParent p1 = new TreeParent("Parent 1");
+			p1.addChild(to1);
+			p1.addChild(to2);
+			p1.addChild(to3);
+
+			TreeObject to4 = new TreeObject("Leaf 4");
+			TreeParent p2 = new TreeParent("Parent 2");
+			p2.addChild(to4);
+
+			TreeParent root = new TreeParent("Root");
+			root.addChild(p1);
+			root.addChild(p2);
+
+			invisibleRoot = new TreeParent("");
+			invisibleRoot.addChild(root);
+		}
+	}
+
+	class ViewLabelProvider extends LabelProvider {
+
+		public String getText(Object obj) {
+			return obj.toString();
+		}
+
+		public Image getImage(Object obj) {
+			String imageKey = ISharedImages.IMG_OBJ_ELEMENT;
+			if (obj instanceof TreeParent)
+				imageKey = ISharedImages.IMG_OBJ_FOLDER;
+			return PlatformUI.getWorkbench().getSharedImages().getImage(imageKey);
+		}
+	}
+
+	class NameSorter extends ViewerSorter {
+	}
+
+	/**
+	 * The constructor.
+	 */
+	public SampleView() {
+	}
+
+	/**
+	 * This is a callback that will allow us to create the viewer and initialize it.
+	 */
+	public void createPartControl(Composite parent) {
+		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		drillDownAdapter = new DrillDownAdapter(viewer);
+		viewer.setContentProvider(new ViewContentProvider());
+		viewer.setLabelProvider(new ViewLabelProvider());
+		viewer.setSorter(new NameSorter());
+		viewer.setInput(getViewSite());
+
+// Create the help context id for the viewer's control
+		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "swtbot.example.view.viewer");
 		getSite().setSelectionProvider(viewer);
 		makeActions();
 		hookContextMenu();
@@ -106,13 +238,17 @@ public class SampleView extends ViewPart {
 	private void fillContextMenu(IMenuManager manager) {
 		manager.add(action1);
 		manager.add(action2);
-		// Other plug-ins can contribute there actions here
+		manager.add(new Separator());
+		drillDownAdapter.addNavigationActions(manager);
+// Other plug-ins can contribute there actions here
 		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
 	}
-	
+
 	private void fillLocalToolBar(IToolBarManager manager) {
 		manager.add(action1);
 		manager.add(action2);
+		manager.add(new Separator());
+		drillDownAdapter.addNavigationActions(manager);
 	}
 
 	private void makeActions() {
@@ -123,9 +259,9 @@ public class SampleView extends ViewPart {
 		};
 		action1.setText("Action 1");
 		action1.setToolTipText("Action 1 tooltip");
-		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
+		action1.setImageDescriptor(
+				PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+
 		action2 = new Action() {
 			public void run() {
 				showMessage("Action 2 executed");
@@ -133,13 +269,13 @@ public class SampleView extends ViewPart {
 		};
 		action2.setText("Action 2");
 		action2.setToolTipText("Action 2 tooltip");
-		action2.setImageDescriptor(workbench.getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
+		action2.setImageDescriptor(
+				PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
 		doubleClickAction = new Action() {
 			public void run() {
-				IStructuredSelection selection = viewer.getStructuredSelection();
-				Object obj = selection.getFirstElement();
-				showMessage("Double-click detected on "+obj.toString());
+				ISelection selection = viewer.getSelection();
+				Object obj = ((IStructuredSelection) selection).getFirstElement();
+				showMessage("Double-click detected on " + obj.toString());
 			}
 		};
 	}
@@ -151,14 +287,14 @@ public class SampleView extends ViewPart {
 			}
 		});
 	}
+
 	private void showMessage(String message) {
-		MessageDialog.openInformation(
-			viewer.getControl().getShell(),
-			"Sample View",
-			message);
+		MessageDialog.openInformation(viewer.getControl().getShell(), "Sample View", message);
 	}
 
-	@Override
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
 	public void setFocus() {
 		viewer.getControl().setFocus();
 	}
